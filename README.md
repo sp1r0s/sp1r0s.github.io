4d3cb5331ecc588bfdc3f8b6356012c59930c396
sp1r0s <sp1r0s@users.noreply.github.com>
2015-12-26 09:26:10 +0000

    Auto-generated commit from deploy app sp1r0s-website-builder

--- README.md
+++ README.md
@@ -3,9 +3,3 @@ My website about technology and stuff
 
 ## Description
 An angular single page app
-
-## Todo
-- [ ] Add Grunt, npm or bower
-- [ ] Add angular and ui router
-- [ ] Start adding articles
-- [ ] Start writing my online cv
--- scripts/app.js
+++ scripts/app.js
@@ -5,4 +5,8 @@
  * @description
  * Main module of the application.
  */
-angular.module('sp1r0s', []);
+angular.module('sp1r0s', ['ui.bootstrap']);
+
+particlesJS.load('particles-js', 'resources/particles-conf.json', function() {
+  console.log('particles-js config loaded');
+});
